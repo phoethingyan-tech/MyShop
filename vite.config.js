6a04c0c1bf202987a3c4da773cb63f3873025ec1
phoethingyan-tech <phoethingyan.tech@gmail.com>
2025-03-22 10:53:47 +0630

    update file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+
+    build: { 
+        outDir: 'public/build', // ဒီလိုသတ်မှတ်ထားပါ 
+        manifest: true, // manifest.json ဖိုင်ထုတ်ဖို့ 
+    },
 });
