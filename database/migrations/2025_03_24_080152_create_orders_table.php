a05c7b8216ffa999837326a802245ce79e066429
phoethingyan-tech <phoethingyan.tech@gmail.com>
2025-03-24 15:22:11 +0630

    update vocher to voucher

--- database/migrations/2025_03_24_080152_create_orders_table.php
+++ database/migrations/2025_03_24_080152_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('vocher_no');
+            $table->string('voucher_no');
             $table->string('price');
             $table->string('qty');
             $table->string('status');
